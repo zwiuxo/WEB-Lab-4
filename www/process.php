9956026323c86ba536c9e8d08bd481d08b71cde6
zwiuxo <im.ssskiller@gmail.com>
2026-04-27 22:04:06 +0300

    create process php

---
+++ www/process.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once 'ApiClient.php';
+
+$name = htmlspecialchars($_POST['user_name'] ?? '');
+
+if (empty($name)) {
+    $_SESSION['errors'] = ["Введите имя для заказа такси"];
+    header("Location: index.php");
+    exit();
+}
+
+$api = new ApiClient();
+
+$url = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=55.75&longitude=37.61&localityLanguage=ru';
+
+// штрафное задание
+$cacheFile = 'api_cache.json';
+$cacheTtl = 300; 
+
+if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < $cacheTtl)) {
+    $apiData = json_decode(file_get_contents($cacheFile), true);
+} else {
+    $apiData = $api->request($url);
+    if (!isset($apiData['error'])) {
+        file_put_contents($cacheFile, json_encode($apiData, JSON_UNESCAPED_UNICODE));
+    }
+}
+
+$_SESSION['user_name'] = $name;
+$_SESSION['api_data'] = $apiData;
+
+setcookie("last_submission", date('Y-m-d H:i:s'), time() + 3600, "/");
+
+header("Location: index.php");
+exit();
